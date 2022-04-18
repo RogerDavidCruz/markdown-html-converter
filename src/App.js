62b50e29d596a68f8276a44033abcb2c5bacf8ea
Roger David Cruz <roger.david.cruz@gmail.com>
2022-04-18 16:13:18 -0400

    frontend markdown to html converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import parse from 'html-react-parser'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    const [value, setValue] = useState('');
+    const [htmlSize, setHtmlSize] = useState(0);
+    const [parsedHtml, setParsedHtml] = useState('');
+
+    const handleChange = event => {
+        setValue(event.target.value);
+    }
+
+    const handleParsedHtml = (html)  => {
+        setHtmlSize(html.length);
+        setParsedHtml(parse(html), html.length);
+    }
+
+    const handleReset = () => {
+        setValue('');
+        setHtmlSize(0);
+        setParsedHtml('');
+    }
+
+    const stringToRegex = (regex, replacement) => {
+        return (str) => {
+            return str.replace(regex, replacement);
+        }
+    }
+    
+    //Matching Markdown syntax with regex rules
+    const headersRegex = /^(#{1,6}) (.*)$/gm;
+
+    const paragraphRegex = /\n+(?!<h)(?!<ul>)(?!<hr)([^\n]+)\n/g;
+
+    const bracketsRegex = /\[([^\[]+)\]\(([^\)]+)\)/g;
+    
+    const horizonLineRegex = /\n((\-{3,})|(={3,}))/g;
+
+    const italicBoldRegex = /(\*{1,2})(.*?)\1/g;
+    
+    
+    //converter functions to change lines into regex
+    const headersconverter = (textAll, octothorp, textContent) => {
+		return '\n<h' + octothorp.trim().length + '>' + textContent + 
+		'</h' + octothorp.trim().length + '>';
+	}
+
+    const paragraphconverter = (textAll, textContent) => '<p>' + textContent + '</p>';
+
+    const urlconverter = (textAll, title, URL) => '<a href="' + URL + '">' + title + '</a>';
+    
+    const horizonLineconverter = textAll => '\n<hr />';
+
+    const italicBoldconverter = (textAll, asterisk, textContent) => {
+        return '<' + (asterisk.trim().length===1 ? 'em' : 'strong') + '>' + 
+		textContent + '</' + (asterisk.trim().length===1 ? 'em' : 'strong') + '>';
+    }
+    
+    //helper functions to finalize string to regex
+    const changeHeaders = stringToRegex(headersRegex, headersconverter);
+
+    const changeParagraphs = stringToRegex(paragraphRegex, paragraphconverter);
+
+    const changeURL = stringToRegex(bracketsRegex, urlconverter);
+
+    const changeHorizontalLine = stringToRegex(horizonLineRegex, horizonLineconverter);
+
+    const changeFontStyle = stringToRegex(italicBoldRegex, italicBoldconverter);
+
+
+    const markdownModification = str => {
+      return changeParagraphs(
+        changeHorizontalLine(changeFontStyle(
+            changeHeaders(changeURL(str)
+            )))
+        );
+    }
+
+    const markToHtmlconverter = str => {
+        return markdownModification('\n' + str + '\n').trim();
+    }
+
+    const html = markToHtmlconverter(value);
+
+    return (
+    <div className="center">
+        <div id="title">
+            <h1>Markdown Format Converter</h1>
+        </div>
+        
+        <div className="boxes">
+            <div className="boxes">
+                <h2>Markdown</h2>
+                <textarea value={value} data-testid="mdText" onChange={handleChange} cols={80} rows={10} />
+            </div>
+            <div className="boxes">
+                <h2>HTML</h2>
+                <div data-testid="newHTML" className="htmlRegion">{html}</div>
+            </div>
+        </div>
+
+		<br/>
+
+        {value ? <button onClick={()=>handleParsedHtml(html)}>
+            Format HTML </button> : ''}
+
+        {(htmlSize !== html.length && htmlSize !== 0) ? 
+            <button className="space" onClick={()=>handleReset()}>
+                Reset</button>:''}
+        
+        <br/>
+		<br/>
+
+        {parsedHtml ? 
+            <div className="boxes">
+                <h2>Display</h2>
+                <div data-testid="parsedHtml" className="displayRegion">{parsedHtml}</div>
+            </div>
+        : ''}
+
     </div>
-  );
+    );  
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
+import { render, fireEvent,queryByTestId} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders successfully', () => {
+    const container = document.createElement('div');
+    act(() => {
+      const root = ReactDOMClient.createRoot(container);
+      root.render(<App />);
+      root.unmount(container);
+    })
+	});
+});
+
+describe('Text region exists', () => {
+    it('valid input field', () => {
+	  const html = render(<App />);
+	  const textarea = html.queryByTestId('mdText');
+      expect(textarea).toBeTruthy();
+    });
+});
+
+describe('Enter sample markdown test #1', () => {
+    it('test markdown converted html', () => {
+	  const html = render(<App />);
+	  const textarea = html.queryByTestId('mdText');
+      fireEvent.change(textarea, { target: { value: "# This is an example format changer \n\n In addition, let us check how a link would convert. \n\n [Example Link](www.sample.com)" } })
+	  const newHTML = html.queryByTestId('newHTML');
+	  expect(newHTML).toHaveTextContent('<h1>This is an example format changer </h1><p> In addition, let us check how a link would convert. </p><p> <a href="www.sample.com">Example Link</a></p>');
+    });
+});
+  
+describe('Enter sample markdown test #2', () => {
+    it('test markdown converted html', () => {
+	  const html = render(<App />);
+	  const textarea = html.queryByTestId('mdText');
+      fireEvent.change(textarea, { target: { value: "# Sample Document\nHello!\n\nThis is sample markdown for the [Mailchimp](https://www.mailchimp.com) homework assignment." } })
+	  const newHTML = html.queryByTestId('newHTML');
+	  expect(newHTML).toHaveTextContent('<h1>Sample Document</h1><p>Hello!</p><p>This is sample markdown for the <a href="https://www.mailchimp.com">Mailchimp</a> homework assignment.</p>');
+    });
+});
+
+describe('Enter sample markdown test #3', () => {
+    it('test markdownconverted html', () => {
+	  const html = render(<App />);
+	  const textarea = html.queryByTestId('mdText');
+      fireEvent.change(textarea, { target: { value: "# Header one\nHello there\n\nHow are you?\nWhat's going on?\n\n## Another Header\n\nThis is a paragraph [with an inline link](http://google.com). Neat, eh?\n\n## This is a header [with a link](http://yahoo.com)" } })
+	  const newHTML = html.queryByTestId('newHTML');
+	  expect(newHTML).toHaveTextContent('<h1>Header one</h1><p>Hello there</p><p>How are you?</p>What\'s going on? <h2>Another Header</h2><p>This is a paragraph <a href="http://google.com">with an inline link</a>. Neat, eh?</p> <h2>This is a header <a href="http://yahoo.com">with a link</a></h2>');
+    });
 });
